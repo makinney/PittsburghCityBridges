727abf6c48468dfd2d12f8e8763bed60465a4156
Michael Kinney <michaelakinney@comcast.net>
2021-12-27 18:23:29 -0500

    Change UI text copy to 'no data' for year built

--- PittsburghCityBridges/Models/BridgeModel.swift
+++ PittsburghCityBridges/Models/BridgeModel.swift
@@ -30,7 +30,7 @@ struct BridgeModel: Identifiable {
     var yearBuilt: String {
         guard let yearBuilt = geoJSON.yearBuilt,
               !yearBuilt.isEmpty else {
-                  return "unknown"
+                  return "no data"
               }
         return yearBuilt
         
