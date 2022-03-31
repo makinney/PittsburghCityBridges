e089b11bccfd77f9751755e7f66523ed806e1d7c
Michael Kinney <michaelakinney@comcast.net>
2022-03-31 16:49:11 -0400

    BridgeDetailView - refactor the zoomable bridge view code into its own function.

--- PittsburghCityBridges/Views/Bridges/BridgeDetailsView.swift
+++ PittsburghCityBridges/Views/Bridges/BridgeDetailsView.swift
@@ -10,7 +10,7 @@ import os
 
 struct BridgeDetailsView: View {
     @ObservedObject var favorites: Favorites
-    @State private var bridgeImageOnly = false
+    @State private var bridgeImageZoomable = false
     @State var bridgeImage = UIImage()
     @State private var dragOffset: CGSize = .zero
     @State private var imageScale: CGFloat = 1.0
@@ -27,10 +27,6 @@ struct BridgeDetailsView: View {
     private let userInterfaceIdiom: UIUserInterfaceIdiom
     private let logger =  Logger(subsystem: AppLogging.subsystem, category: AppLogging.debugging)
     
-    private func totalOffset(offset: CGSize, by: CGSize) -> CGSize {
-        CGSize(width: offset.width + by.width, height: offset.height + by.height)
-    }
-    
     init(bridgeModel: BridgeModel, favorites: Favorites) {
         self.bridgeModel = bridgeModel
         bridgeImageSystem = BridgeImageSystem()
@@ -41,7 +37,7 @@ struct BridgeDetailsView: View {
     var dragGesture: some Gesture {
         DragGesture()
             .onChanged {
-                if self.bridgeImageOnly {
+                if self.bridgeImageZoomable {
                     self.dragOffset = self.totalOffset(offset: self.initialDragOffset, by: $0.translation)
                 }
             }
@@ -71,41 +67,8 @@ struct BridgeDetailsView: View {
     
     var body: some View {
         VStack {
-            if bridgeImageOnly {
-                GeometryReader { geometry in
-                    VStack(alignment: .center) {
-                        Image(uiImage: bridgeImage)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .matchedGeometryEffect(id: "BridgeView", in: bridgeAnimations)
-                            .frame(maxWidth: geometry.frame(in: .global).width, maxHeight: geometry.frame(in: .global).height)
-                            .scaleEffect(imageScale)
-                            .animation(.easeInOut, value: imageScale)
-                            .offset(dragOffset)
-                            .animation(.easeInOut, value: dragOffset)
-                            .gesture(magGesture.simultaneously(with: dragGesture))
-                            .gesture(dblTapToZoomInGesture)
-                            .toolbar {
-                                ToolbarItem(placement: .navigationBarTrailing) {
-                                    Button("Done") {
-                                        self.dragOffset = .zero
-                                        self.initialDragOffset = .zero
-                                        self.imageScale = 1.0
-                                        self.imageMagnification = 1.0
-                                        self.bridgeImageOnly = false
-                                    }
-                                }
-                            }
-                        Spacer()
-                    }
-                }
-                .background(Color.pbBgnd)
-                .onAppear {
-                    UIScrollView.appearance().bounces = false
-                }
-                .onDisappear {
-                    UIScrollView.appearance().bounces = true
-                }
+            if bridgeImageZoomable {
+                bridgeImageZoomableView()
             } else {
                 GeometryReader { geometry in
                     ScrollView(showsIndicators: false) {
@@ -143,7 +106,7 @@ struct BridgeDetailsView: View {
                             .gesture(
                                 TapGesture(count: 1)
                                     .onEnded{
-                                        self.bridgeImageOnly = true
+                                        self.bridgeImageZoomable = true
                                     }
                             )
                             Text(bridgeModel.builtHistory())
@@ -193,7 +156,44 @@ struct BridgeDetailsView: View {
         .navigationBarTitleDisplayMode(.inline)
         .navigationTitle("\(bridgeModel.name)")
         .background(Color.pbBgnd)
-        .animation(.default, value: bridgeImageOnly)
+        .animation(.default, value: bridgeImageZoomable)
+    }
+    
+    private func bridgeImageZoomableView() -> some View {
+        return GeometryReader { geometry in
+            VStack(alignment: .center) {
+                Image(uiImage: bridgeImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .matchedGeometryEffect(id: "BridgeView", in: bridgeAnimations)
+                    .frame(maxWidth: geometry.frame(in: .global).width, maxHeight: geometry.frame(in: .global).height)
+                    .scaleEffect(imageScale)
+                    .animation(.easeInOut, value: imageScale)
+                    .offset(dragOffset)
+                    .animation(.easeInOut, value: dragOffset)
+                    .gesture(magGesture.simultaneously(with: dragGesture))
+                    .gesture(dblTapToZoomInGesture)
+                    .toolbar {
+                        ToolbarItem(placement: .navigationBarTrailing) {
+                            Button("Done") {
+                                self.dragOffset = .zero
+                                self.initialDragOffset = .zero
+                                self.imageScale = 1.0
+                                self.imageMagnification = 1.0
+                                self.bridgeImageZoomable = false
+                            }
+                        }
+                    }
+                Spacer()
+            }
+        }
+        .background(Color.pbBgnd)
+        .onAppear {
+            UIScrollView.appearance().bounces = false
+        }
+        .onDisappear {
+            UIScrollView.appearance().bounces = true
+        }
     }
     
     func makeMapView(_ bridgeModel: BridgeModel) -> some View {
@@ -230,7 +230,10 @@ struct BridgeDetailsView: View {
                 Spacer()
             }
         }
-
+    }
+    
+    private func totalOffset(offset: CGSize, by: CGSize) -> CGSize {
+        CGSize(width: offset.width + by.width, height: offset.height + by.height)
     }
 }
 
